0181aa3fed3f5260e22caa61d7e05625b3e0cd2f
Mohamed-Soighir-attoumani <131267845+Mohamed-Soighir-attoumani@users.noreply.github.com>
2025-08-30 16:28:01 +0300

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 /**
  * Protège les routes privées :
  * - exige un token en localStorage
  * - sinon redirige vers /login en conservant la destination
+ * Supporte les 2 usages : wrapper avec children OU route parent avec <Outlet/>
  */
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
@@ -13,7 +14,7 @@ const PrivateRoute = ({ children }) => {
   if (!token) {
     return <Navigate to="/login" replace state={{ from: location }} />;
   }
-  return children;
+  return children ?? <Outlet />;
 };
 
 export default PrivateRoute;

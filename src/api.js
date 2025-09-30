88c2ec93310a23c6bd4244e35dd57f07d88a6e1d
Mohamed-Soighir-attoumani <131267845+Mohamed-Soighir-attoumani@users.noreply.github.com>
2025-09-30 04:17:46 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,12 +2,11 @@
 import axios from "axios";
 import { API_URL } from "./config";
 
-// ✅ On supprime le /api final si présent pour éviter /api/api/...
+// On supprime /api final si présent pour éviter /api/api/...
 const ROOT_URL = API_URL.replace(/\/api\/?$/, "");
 
 const api = axios.create({ baseURL: ROOT_URL });
 
-// Ajoute automatiquement le token admin si présent (localStorage)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {

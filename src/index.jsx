fdddf200a99c19c32dbb37c2a4dc37d16ec457ce
Mohamed-Soighir-attoumani <131267845+Mohamed-Soighir-attoumani@users.noreply.github.com>
2025-10-07 22:29:53 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "react-quill/dist/quill.snow.css"; 
+import "react-quill/dist/quill.core.css";
 import App from "./App";
 import "./index.css";
 
